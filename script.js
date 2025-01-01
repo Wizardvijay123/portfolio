a319b6c73ee1b22e5e050c1d013ff2c1efc6a67a
unknown <vijay9787321142@gmail.com>
2025-01-01 11:14:06 +0530

    1.1.25

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js - Empty file for now, you can add JavaScript functionality as needed
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Vijay's Portfolio!");
+});
